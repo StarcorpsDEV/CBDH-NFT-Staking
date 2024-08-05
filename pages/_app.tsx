d02eba6708a0bb998732e51e6a2f91a521903247
StarsorpsDEV <starcorps@protonmail.com>
2024-08-06 00:56:07 +0300

    commit 5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 // This is the chain your dApp will work on.
 const activeChain = "polygon";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    
     <ThirdwebProvider
-      activeChain={activeChain}
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+    activeChain={activeChain}
+    clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
     >
+      <Analytics/>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
