b77c36e1724f09fac8a3707d973e3b64767b1f84
StarsorpsDEV <starcorps@protonmail.com>
2024-08-06 15:45:45 +0300

    commit 5

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -3,42 +3,42 @@ export const DMFSBnftDropContractAddress =
 export const DMFSBtokenContractAddress =
 "0xd5bd4BeEf25b5952a1462e92C73CF8dc5870D683";
 export const DMFSBstakingContractAddress =
-"0xAf345DF289a6610703d1B5AfC10BFa9a900Afcd2";
+"0x91c74234b00454b147DffC46dcCC7ff25e9fEBb2";
 
 export const RRFSBnftDropContractAddress =
 "0x852CEbD22E2DedC5844F594D70fd2Bd44704De48";
 export const RRFSBtokenContractAddress =
 "0x7F8A9B97d252E76919eb70fe48cE78835af7a6D9";
 export const RRFSBstakingContractAddress =
-"0x4C78669dcfb33176C575248671721d98D3254d94";
+"0x28b968eD8662a95E19E0f7166E843917627656c5";
 
 export const RBFSBnftDropContractAddress =
 "0x8Dbfce5096567259a8839C80c6ca15e169381560";
 export const RBFSBtokenContractAddress =
 "0x512037B66BacD1b020AA439e159934095Df8A2C8";
 export const RBFSBstakingContractAddress =
-"0x227f5bCC428ca338702F2B4f9a7Fc1a25bEB5F0B";
+"0xFC5B83E3F0d5da3dA0f9867341eA20D19240a341";
 
 export const RGFSBnftDropContractAddress =
 "0x5dF79DD2f30269b1065f8D3497919e1E49702305";
 export const RGFSBtokenContractAddress =
 "0x645Cb7A052b178cbB4eDCF9bc4A28F9Fcc67b5A7";
 export const RGFSBstakingContractAddress =
-"0x9bEd63b45C8110031b00Ec66aa08B3d4E0106510";
+"0x8891e47D1d440B549CbB195D1B026a86b2B722cA";
 
 export const RPFSBnftDropContractAddress =
 "0x303060B9ec801D809C660687D1e5BAA540b40B62";
 export const RPFSBtokenContractAddress =
 "0x6238F788a7291f5D1b4786F7CE0835468cd84fBa";
 export const RPFSBstakingContractAddress =
-"0xB6e49b831bE04d1cF8b3AAd5bb19b1241299F87c";
+"0x49B936c1D58D4b3074C3eEAbacB91e0828759718";
 
 export const SMFSBnftDropContractAddress =
 "0x37B1c0f1648aA496b04657c8cA639e11731D737b";
 export const SMFSBtokenContractAddress =
 "0x1fD0E55C66B42be30793Eae30DbCdB8A8184DF42";
 export const SMFSBstakingContractAddress =
-"0xBbDDCC632D8C746fB538bA8312969d08a8c99237";
+"0x418306266065bDd5e1528e9b183C0e553E706750";
 
 export const RFLBanftDropContractAddress =
 "0xa41573F9A4791be31D538FA5916d1Ee8B849eC52";
